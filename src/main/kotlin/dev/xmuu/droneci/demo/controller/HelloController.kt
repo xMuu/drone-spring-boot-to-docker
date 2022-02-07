005e7c1247cc47a02780155eec6fb74f2e70253b
xMuu <imxmuu@gmail.com>
2022-02-08 00:22:44 +0800

    remove log

--- src/main/kotlin/dev/xmuu/droneci/demo/controller/HelloController.kt
+++ src/main/kotlin/dev/xmuu/droneci/demo/controller/HelloController.kt
@@ -1,18 +1,14 @@
 package dev.xmuu.droneci.demo.controller
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
 
-    private val logger = LoggerFactory.getLogger(javaClass)
-
     @RequestMapping("/")
     fun index(): String {
-        logger.info("Hello World")
-        return "Hello, world!"
+        return "Hello, World!"
     }
 
 }
