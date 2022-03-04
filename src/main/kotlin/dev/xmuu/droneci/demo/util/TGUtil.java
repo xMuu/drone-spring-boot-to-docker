b2c5c0d3376dc0f9e7d1bb64d9e56b09363acbd3
xMuu <imxmuu@gmail.com>
2022-03-04 14:02:50 +0800

    send message on boot!

---
+++ src/main/kotlin/dev/xmuu/droneci/demo/util/TGUtil.java
@@ -0,0 +1,60 @@
+package dev.xmuu.droneci.demo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Component
+public class TGUtil {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Value("${config.telegram.token}")
+    public String token;
+
+    @Value("${config.telegram.chat-id}")
+    public String chatId;
+
+    private String getBaseUrl() {
+        return "https://api.telegram.org/bot" + token + "/";
+    }
+
+    private String getSendMessageUrl() {
+        return getBaseUrl() + "sendMessage?chat_id=" + chatId + "&text={1}";
+    }
+
+    private RestTemplate getDefaultTemplate() {
+        RestTemplate template = new RestTemplate();
+        template.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) throws IOException {
+                return response.getStatusCode().isError();
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                logger.info("Error: " + response.getStatusCode());
+            }
+        });
+        return template;
+    }
+
+    public void sendMessage(String message) {
+        RestTemplate template = getDefaultTemplate();
+        String result = template.getForObject(getSendMessageUrl(), String.class, message);
+        logger.info("TG sendMessage result: {}", result);
+    }
+
+    @PostConstruct
+    public void sendOnBoot() {
+        sendMessage("Drone CI Demo is up and running!");
+    }
+
+}
